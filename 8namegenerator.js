487022a216ff1fa108cff9c63996e6aab39d4c14
AyushiRai28 <ayushi28.vns@gmail.com>
2026-05-14 18:33:29 +0530

    arrayss

---
+++ 8namegenerator.js
@@ -0,0 +1,28 @@
+/*
+Create a business name generator by combining list of adjectives and shop name and another word.
+
+adjectives:
+crazy
+amazing
+fire
+
+shop name:
+engine 
+foods
+garments
+
+another word:
+bros
+limited
+hub
+ */
+
+// let adj = Math.random("crazy" , "amazing" , "fire")
+// let shop = Math.random("engine" , "foods" , "garment ")
+// let extra = Math.random("bros" , "limited" , "hub")
+
+// // const new_name = adj , shop , extra ;
+
+// console.log(`The random Business Name suggested is  ${adj} ${shop} ${extra} `)
+
+// the above code doesn't works. Lets see Harrys solution
