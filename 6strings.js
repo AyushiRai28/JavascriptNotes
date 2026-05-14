9480c2955f98e63b280b60e79212363dd239436a
AyushiRai28 <ayushi28.vns@gmail.com>
2026-05-14 12:43:43 +0530

    faultycalc

---
+++ 6strings.js
@@ -0,0 +1,18 @@
+let a = "Ayushi";
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+console.log(a[5])
+console.log(a.length)
+console.log(a.toUpperCase())
+console.log(a.toLowerCase())
+
+let you = "anushree"
+let me = "ayushi"
+console.log(`I know it very well that ${you} like ${me}`)
+console.log(you.slice(1,6))
+console.log(you.slice(1))
+console.log(you.concat(me, "popsyyy"))
+
---
+++ faultyCalculator.js
@@ -0,0 +1,36 @@
+/*
+create a faulty calculator using javascript
+
+this faulty calculator does following:
+1. It takes two numbers  as input from the user
+2, It performs wrong operations as follows:
++ ---> -
+* ---> +
+- ---> /
+/ ----> **
+
+it performs wrong operations 10% of the times
+ */
+
+let random=Math.random()
+let a = prompt("Enter first number")
+let c = prompt("Enter operation")
+let b = prompt("Enter second number")
+
+let obj = {
+    "+" : "-",
+    "*" : "+",
+    "-" : "/",
+    "/" : "**",
+}
+
+console.log(random)
+if(random>0.1){
+    alert(`The result is ${eval(`${a} ${c} ${b}`)}`)
+    //correct calculation
+}
+else{
+    c = obj[c]
+    alert(`The result is ${eval(`${a} ${c} ${b}`)}`)
+    //wrong calculation
+}
