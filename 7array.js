27e3d6998dbdff0425d3fd7a3a3edc23b117d656
AyushiRai28 <ayushi28.vns@gmail.com>
2026-05-14 18:52:18 +0530

    array

--- 7array.js
+++ 7array.js
@@ -1,4 +1,12 @@
 let arr = [1,2,3,4,5]
 
+arr[0]=666;
+
 console.log(arr);
 console.log(arr.length);
+console.log(arr[3]);
+console.log(arr.toString());
+console.log(arr.join(" and "));
+console.log(arr.pop());
+console.log(arr.push(100));
+console.log(arr);
