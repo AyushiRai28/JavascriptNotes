fbadf329cca6f6ac9fc1ba63522bc7424e35fdee
AyushiRai28 <ayushi28.vns@gmail.com>
2026-05-14 09:52:21 +0530

    variables

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
-console.log("Hey welcome to javascript")
+console.log("Hey welcome to javascript");
 var a = 5;
 var b = 6;
 var c = "haary";
 
+// var can not start with a number
+// Harry and harry are diff
 
-console.log(a+b+8)
-console.log(typeof a,  typeof b, typeof c)
+console.log(a);
+console.log(a+b+8);
+console.log(typeof a,  typeof b, typeof c);
+
+a = a+1;
+const a1 = 6;
+//a1 = a1 + 6 //this wont work unlike 'a' as it is a constant
+
+
+
+//let
+let z = 7;
+{
+    let z = 66;
+    console.log(z);
+
+}
+console.log(z)
+
+// if this was done with var , it would show the latest given value, var is globally swwitched. Let is locally.
