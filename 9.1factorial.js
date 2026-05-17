9be770d2841fda58efb44bc278d80f1d4bb2f7b5
AyushiRai28 <ayushi28.vns@gmail.com>
2026-05-17 19:52:14 +0530

    factorial

--- 9.1factorial.js
+++ 9.1factorial.js
@@ -4,9 +4,20 @@ function factorial(number){
     console.log(arr.slice(1,)) //to not include 0
     let c = arr.slice(1,).reduce((a,b)=>{
                                     return a*b})
-                                    
-                                
+    return c
+  }
+
+console.log(factorial(a)) // creates an arr of numbers  from 1 to a
+
+
+
+
+function FacFor(number){
+    let fac = 1;
+    for (let i = 1; i <= number; i++) {
+      fac = fac*i
+    }
+    return fac
 }
 
-factorial(a) // creates an arr of numbers  from 1 to a
-console.log(c)
+console.log(FacFor(6))
