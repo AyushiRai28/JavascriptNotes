2aaac2b62c9dfa6b968a9a1b324c6aab37e41f73
AyushiRai28 <ayushi28.vns@gmail.com>
2026-05-20 19:13:35 +0530

    ch4

--- exercises/ch3.js
+++ exercises/ch3.js
@@ -6,6 +6,45 @@ let obj = {
     akash: 7
 };
 
-for (let key in obj) {
-    console.log(key + " : " + obj[key]);
+let keys = Object.keys(obj);
+for (let i = 0; i < keys.length; i++) {
+    console.log(keys[i] + " : " + obj[keys[i]]);
 }
+
+
+// using for in
+for (const key in obj) {
+    console.log(key + " : " + obj[key]);
+    
+    
+}
+
+
+//Q. Program to show try agian untill correct number is placed
+let correctNumber = 7;
+let userInput = 5
+
+while (userInput != correctNumber) {
+    userInput = prompt("Enter the correct number:");
+}
+
+alert("You entered the correct number!");
+
+//Write a function to find mean of 5 numiers
+function mean(p,q,r,s,t) {
+    m = (p+q+r+s+t)/5
+    console.log(`the mean is: ${m}`)
+}
+
+mean(2,2,2,2,2)
+
+//or
+let a = 10;
+let b = 20;
+let c = 30;
+let d = 40;
+let e = 50;
+
+let mean = (a + b + c + d + e) / 5;
+
+console.log("Mean is:", mean);
---
+++ exercises/ch4.js
@@ -0,0 +1,6 @@
+function mean(p,q,r,s,t) {
+    m = (p+q+r+s+t)/5
+    console.log(`the mean is: ${m}`)
+}
+
+mean(2,2,2,2,2)
