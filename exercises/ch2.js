16cd61c4c94041812b146db1d97ddd1936fafdea
AyushiRai28 <ayushi28.vns@gmail.com>
2026-05-20 12:19:32 +0530

    ch2

---
+++ exercises/ch2.js
@@ -0,0 +1,12 @@
+//Q. Use logical operator to find whether the age of A perion lies between 10 and 20,
+let a
+
+if(a>10 && a<20){
+    console.log("Valid age")
+}
+
+else{
+    console.log("Invalid Age")
+}
+
+//Q. Demonstrate the use of switch case statements in Javascript
