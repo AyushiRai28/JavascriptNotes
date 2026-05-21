3defe90be1e2857c39013354fc3bd966c874038d
AyushiRai28 <ayushi28.vns@gmail.com>
2026-05-21 21:57:13 +0530

    ch7

--- exercises/ch7.js
+++ exercises/ch7.js
@@ -1,1 +1,29 @@
+//Q. Create a navbar and change the color of it background to red
+document.getElementsByClassName("nav")[0].style.backgroundColor = "red" ;
+//or
+document.querySelector(".nav").style.backgroundColor = "red";
 
+
+
+//Q. Create a table without tbody Now use "view рage Source" button to check whether it has a tbody or not
+// not shown in view sourse but shoen in inspect 
+/*Important Concept
+Browser automatically adds <tbody>
+Even if you don’t write it in HTML
+It appears in DOM (Inspect), not in source */
+
+
+
+//Q. Create an element with 3 children Now change the color of first and last element to green
+document.getElementsByClassName("box")[0].style.backgroundColor = "green";
+document.getElementsByClassName("box")[2].style.backgroundColor = "green";
+//or
+let parent = document.getElementsByClassName("container");
+parent.firstElementChild.style.color = "green";
+parent.lastElementChild.style.color = "green";
+
+//Q. Write code to change background of all <li> tags to cyan
+let items = document.getElementsByTagName("li");
+for (let i = 0; i < items.length; i++) {
+    items[i].style.backgroundColor = "cyan";
+}
