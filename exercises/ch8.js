a9d3176c2379eede7d9ef963d0e60f81cf9f9f7a
AyushiRai28 <ayushi28.vns@gmail.com>
2026-05-23 10:33:19 +0530

    ch8

---
+++ exercises/ch8.js
@@ -0,0 +1,13 @@
+
+
+        document.getElementById("btn1").addEventListener("click", function() {
+            alert("You clicked Button 1!");
+        });
+
+        document.getElementById("btn2").addEventListener("click", function() {
+            alert("You clicked Button 2!");
+        });
+
+        document.getElementById("btn3").addEventListener("click", function() {
+            alert("You clicked Button 3!");
+        });
