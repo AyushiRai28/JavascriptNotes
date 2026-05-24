91e0be53700f5a5acc15d5167dd65bb02e286025
AyushiRai28 <ayushi28.vns@gmail.com>
2026-05-24 14:09:14 +0530

    commit

--- learnings/15callbacks.js
+++ learnings/15callbacks.js
@@ -1,1 +1,12 @@
-console.log("meow")
+
+const callback = (arg)=>{
+    console.log(arg)
+}
+
+const loadScript = (src , callback) => {
+  let sc = document.createElement("script");
+  sc.src =  src;
+  sc.onload = callback("harry")
+  document.head.append(sc)
+}
+ 
