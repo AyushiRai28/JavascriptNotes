ba3a34705e5987b01dc9c4b48d2e1b87137af1f9
AyushiRai28 <ayushi28.vns@gmail.com>
2026-05-25 13:29:42 +0530

    promise2

--- learnings/16prom2.js
+++ learnings/16prom2.js
@@ -12,7 +12,23 @@ let prom1 = new Promise((resolve, reject) =>{
     },1000);}
 })
 
-prom1.then((a)=>{
+
+
+let prom2 = new Promise((resolve, reject) =>{
+    let a = Math.random();
+    if (a<0.5){
+
+        reject("No random number was not supporting you 2")
+    }
+    else{setTimeout(() => {
+        console.log("Yes I am done 2")
+        resolve("harry 2")
+    },1000);}
+})
+
+let p3 = Promise.all([prom1 , prom2])
+
+p3.then((a)=>{
     console.log(a)
 }).catch((err)=>{console.log(err);
 })
