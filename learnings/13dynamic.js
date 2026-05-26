52ee1c5ffc2821e96b5290238e81da9823323086
AyushiRai28 <ayushi28.vns@gmail.com>
2026-05-26 13:04:48 +0530

    dynamic

--- learnings/13dynamic.js
+++ learnings/13dynamic.js
@@ -1,5 +1,25 @@
-function createCard(title , cName , views, monthsOld, duration, tumbnail){
-    
+function createCard(title , cName , views, monthsOld, duration, thumbnail){
+    let viewStr
+    if(views<1000){
+        viewStr = views
+    }
+    else if(views>1000 && views<1000000){viewStr = views/1000 + "K"}
+    else if(views>1000000){viewStr = views/1000000 + "M"}
+    let html =`
+  <div class="card">
+            <div class="image">
+                <img src="${thumbnail}">
+                <div class="capsule">${duration}</div>
+            </div>
+            <div class="text">
+                <h1>${title}</h1>
+                <p>${cName}. ${viewStr} views . ${monthsOld} months ago</p>
+            </div>
+        </div>
+        `
+ document.querySelector(".container").insertAdjacentHTML("beforeend", html);
+// document.querySelector(".container").innerHTML = document.querySelector(".container").innerHTML + html
+
 }
 
-createCard("Introduction to backend" , "COdewithHarry", 560000, 7, "31.22" , "image.png")
+createCard("Introduction to backend" , "COdewithHarry", 560000, 7, "31.22" , "13image.png")
