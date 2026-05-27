60a51f97ac39098a5ceb47b298c332e463176b1d
AyushiRai28 <ayushi28.vns@gmail.com>
2026-05-27 19:53:55 +0530

    error

--- learnings/16error.js
+++ learnings/16error.js
@@ -1,2 +1,14 @@
-let a = prompt("Enter a number : ")
-let b = prompt("ENger second number")
+let a = Number(prompt("Enter first number : "))
+let b = Number(prompt("Enter second number"))
+
+// javascript has a forgiving nature , it wont show error so if you want it to show error you can throw error   
+if(isNaN(a) || isNaN(b)){
+    throw SyntaxError("Sorry this is not allowed !!")
+}
+let sum = a + b
+// or
+let sum1 = parseInt(a) + parseInt(b)
+
+console.log("the sum is ", sum)
+
+// if you wont put number , it would take it as string
