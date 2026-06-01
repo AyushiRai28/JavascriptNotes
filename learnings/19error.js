b5d59c939aab2d8b26916acecd31837af15fe0b7
AyushiRai28 <ayushi28.vns@gmail.com>
2026-06-01 10:15:30 +0530

    try catch

--- learnings/19error.js
+++ learnings/19error.js
@@ -1,6 +1,7 @@
 let a = Number(prompt("Enter first number : "))
 let b = Number(prompt("Enter second number"))
 
+// if you wont put number , it would take it as string
 // javascript has a forgiving nature , it wont show error so if you want it to show error you can throw error   
 if(isNaN(a) || isNaN(b)){
     throw SyntaxError("Sorry this is not allowed !!")
@@ -9,6 +10,11 @@ let sum = a + b
 // or
 let sum1 = parseInt(a) + parseInt(b)
 
-console.log("the sum is ", sum)
+// console.log("the sum is ", sum)
+//initially we will write this , lets try something else
+try {
+    console.log("the sum is ", sum*x)
+} catch (error) {
+    console.log("error aa gya bhaiii")
+}    
 
-// if you wont put number , it would take it as string
