1bfc7fde3cd7e06b186f27e0e1c73083c0945805
AyushiRai28 <ayushi28.vns@gmail.com>
2026-06-03 20:22:45 +0530

    hackers terminal

--- learnings/15terminal.js
+++ learnings/15terminal.js
@@ -1,21 +1,34 @@
-let text = ["initializing hacking...",
-     "reading your files...",
-     "password files detected...",
-    "sending all pswrds and prsnl files to seerver...",
-    " cleaning up..."]
-
-const randomDelay = ()=>{
-    return new Promise((resolve, reject)=>{
-        timeout = 1 + 6*Math.random()
-        setTimeout(() =>{
-            resolve()
-        }, timeout);
-    })
-}   
-for (const item of text){
-    addItem(item)
-}   
-
-const addItem = async ()=>{
+setInterval(() => {
     
+}, 7000);
+
+const addItem = async (item)=>{
+    await randomDelay();
+    let div = document.createElement("div");
+    div.innerHTML = item;
+    document.body.append(div)}
+    
+    
+    const randomDelay = ()=>{
+        return new Promise((resolve, reject)=>{
+            let timeout = 1 + 6*Math.random()
+            setTimeout(() =>{
+                resolve()
+            }, timeout*1000);
+        })
+    }
+let text = ["initializing hacking",
+    "password files detected",
+    "cleaning up",
+    "sending all pswrds and prsnl files to seerver",
+    "reading your files",]
+ 
+
+
+const run = async () => {
+    for (const item of text) {
+        await addItem(item);
+    }
 }
+run();
+
