c15d5f22b716b53c7d827b0cf121d4a24ad29874
AyushiRai28 <ayushi28.vns@gmail.com>
2026-06-03 16:52:51 +0530

    terminal

---
+++ learnings/15terminal.js
@@ -0,0 +1,13 @@
+let text = ["initializing hacking...",
+     "reading your files...",
+     "password files detected...",
+    "sending all pswrds and prsnl files to seerver...",
+    " cleaning up..."]
+
+for (const item of text){
+    addItem(item)
+}   
+
+const addItem = async ()=>{
+    
+}
